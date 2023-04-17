3e1480c18bea4a2a0adda7162136c92729584668
Kimani Kabiria <kabiriakimani@beunbxd.com>
2023-04-17 16:22:17 +0300

    Initial FastApi setup & Tesseract ocr for national ID

---
+++ main.py
@@ -0,0 +1,42 @@
+import base64
+import io
+import json
+import logging
+import sys
+import uuid
+from io import BytesIO
+from typing import List
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+from PIL import Image
+from starlette.responses import Response
+
+from ocr import recognizer
+
+# FastAPI
+app = FastAPI(
+    title="CO-OP Bank KYC Validator",
+    description="""Visit port 8088/docs for the FastAPI documentation.""",
+    version="0.0.1",
+)
+
+# Base64 Decoder
+def base64_encode_img(img):
+    buffered = BytesIO()
+    img.save(buffered, format="PNG")
+    buffered.seek(0)
+    img_byte = buffered.getvalue()
+    encoded_img = "data:image/png;base64," + base64.b64encode(img_byte).decode()
+    return encoded_img
+
+@app.get("/")
+async def root():
+    return {"message": "CO-OP Bank KYC Validator"}
+
+@app.post("/extract/idno")
+def process_ocr(file: UploadFile = File(...)):
+    file_bytes = file.file.read()
+    image = Image.open(BytesIO(file_bytes))
+    return recognizer.recognize(image)
+
---
+++ ocr/recognizer.py
@@ -0,0 +1,53 @@
+from PIL import Image
+import pytesseract
+import cv2
+import re
+import numpy as np
+import json
+import csv
+from io import StringIO
+
+def get_grayscale(img):
+    tmp_image = np.array(img)
+    return cv2.cvtColor(tmp_image, cv2.COLOR_BGR2GRAY)
+
+# noise removal
+def remove_noise(img):
+    tmp_image = np.array(img)
+    return cv2.medianBlur(tmp_image,5)
+
+def threshold(img):
+    return cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+def recognize(img, lang="eng"):
+    """Process a PIL image."""
+    gray_img = get_grayscale(img)
+
+    thresh = threshold(gray_img)
+
+    noisy_img = remove_noise(thresh)
+
+    new_image = Image.fromarray(noisy_img)
+
+    custom_config = r'-l eng --oem 3 --psm 6'
+    extracted = pytesseract.image_to_string(img, config=custom_config)
+
+    d = pytesseract.image_to_data(img)
+    print(d)
+    tsv_data = csv.DictReader(StringIO(d), delimiter='\t')
+    d = [row for row in tsv_data]
+
+    digit_pattern = '<[B8O0]{2}(\d+)'
+    
+    extracted_text = []
+    
+    for row in d:
+        if int(float(row['conf'])) > 60:
+            match = re.search(digit_pattern, row['text'])
+            if match:
+                extracted_text.append(match.group(1))
+    
+    if not extracted_text:
+        return {"status": "error", "message": "Kindly retry"}
+    
+    return {"status": "success", "message": "" .join(extracted_text)}
